1ba60a801ba5a5d7100e89e326abf64098e0aaba
devbilly789-dotcom <devbilly789@gmail.com>
2026-03-05 23:37:08 +0300

    Update urls.py

--- revision/urls.py
+++ revision/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.shortcuts import redirect
 from . import views
 
+# Root redirect
+def root_redirect(request):
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+    else:
+        return redirect('login')  # redirect guests to login
+
 urlpatterns = [
+    path("", root_redirect, name="root"),
     path("register/", views.register_view, name="register"),
+    path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("payment/", views.payment_view, name="payment"),
     path("waiting/", views.waiting_view, name="waiting"),
     path("dashboard/", views.dashboard_view, name="dashboard"),
